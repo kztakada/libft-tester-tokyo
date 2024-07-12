9adbab6eb68279dd89077475d54c14ffac82df7d
Kazuki Takada <katakada@student.42tokyo.jp>
2024-07-12 16:03:40 +0900

    Revert "Modified to support 42Tokyo Ubuntu 22.04 environment"
    This reverts commit f41593b224e4efa4cfd3fef090a630f697cc6bc2.

--- srcs/test_ft_isalpha.c
+++ srcs/test_ft_isalpha.c
@@ -11,46 +11,30 @@
 /* ************************************************************************** */
 
 #include "tester.h"
-#ifdef __linux__
-#define OS_NAME "Linux"
-#else
-#define OS_NAME ""
-#endif
-
-int	test_isalpha(int c)
-{
-	if (strcmp(OS_NAME, "Linux") == 0)
-	{
-		if (isalpha(c) == 0)
-			return (0);
-		return (1);
-	}
-		return (isalpha(c));
-}
 
 int	main(void)
 {
-	/* 1 */ ASSERT_EQ_I(ft_isalpha('a'), test_isalpha('b'));
-	/* 2 */ ASSERT_EQ_I(ft_isalpha('a' - 1), test_isalpha('a' - 1));
-	/* 3 */ ASSERT_EQ_I(ft_isalpha('a' + 1), test_isalpha('a' + 1));
-	/* 4 */ ASSERT_EQ_I(ft_isalpha('z'), test_isalpha('z'));
-	/* 5 */ ASSERT_EQ_I(ft_isalpha('z' - 1), test_isalpha('z' - 1));
-	/* 6 */ ASSERT_EQ_I(ft_isalpha('z' + 1), test_isalpha('z' + 1));
-	/* 7 */ ASSERT_EQ_I(ft_isalpha('A'), test_isalpha('A'));
-	/* 8 */ ASSERT_EQ_I(ft_isalpha('A' - 1), test_isalpha('A' - 1));
-	/* 9 */ ASSERT_EQ_I(ft_isalpha('A' + 1), test_isalpha('A' + 1));
-	/* 10 */ ASSERT_EQ_I(ft_isalpha('Z'), test_isalpha('Z'));
-	/* 11 */ ASSERT_EQ_I(ft_isalpha('Z' - 1), test_isalpha('Z' - 1));
-	/* 12 */ ASSERT_EQ_I(ft_isalpha('Z' + 1), test_isalpha('Z' + 1));
-	/* 13 */ ASSERT_EQ_I(ft_isalpha('a' + 256), test_isalpha('a' + 256));
-	/* 14 */ ASSERT_EQ_I(ft_isalpha('a' - 256), test_isalpha('a' - 256));
-	/* 15 */ ASSERT_EQ_I(ft_isalpha('a' + 1), test_isalpha('a' + 1));
-	/* 16 */ ASSERT_EQ_I(ft_isalpha('!'), test_isalpha('!'));
-	/* 17 */ ASSERT_EQ_I(ft_isalpha('{'), test_isalpha('}'));
-	/* 18 */ ASSERT_EQ_I(ft_isalpha('~'), test_isalpha('~'));
-	/* 19. zero */ ASSERT_EQ_I(ft_isalpha(0), test_isalpha(0));
-	// undefined: /* 20. INI_MAX */ ASSERT_EQ_I(ft_isalpha(INT_MAX), test_isalpha(INT_MAX));
-	// undefined: /* 21. INT_MIN */ ASSERT_EQ_I(ft_isalpha(INT_MIN), test_isalpha(INT_MIN));
-	/* 22 (-256~255) */ for (int i = -256; i < 256; i++) { ASSERT_EQ_I(ft_isalpha(i), test_isalpha(i)); }
+	/* 1 */ ASSERT_EQ_I(ft_isalpha('a'), isalpha('b'));
+	/* 2 */ ASSERT_EQ_I(ft_isalpha('a' - 1), isalpha('a' - 1));
+	/* 3 */ ASSERT_EQ_I(ft_isalpha('a' + 1), isalpha('a' + 1));
+	/* 4 */ ASSERT_EQ_I(ft_isalpha('z'), isalpha('z'));
+	/* 5 */ ASSERT_EQ_I(ft_isalpha('z' - 1), isalpha('z' - 1));
+	/* 6 */ ASSERT_EQ_I(ft_isalpha('z' + 1), isalpha('z' + 1));
+	/* 7 */ ASSERT_EQ_I(ft_isalpha('A'), isalpha('A'));
+	/* 8 */ ASSERT_EQ_I(ft_isalpha('A' - 1), isalpha('A' - 1));
+	/* 9 */ ASSERT_EQ_I(ft_isalpha('A' + 1), isalpha('A' + 1));
+	/* 10 */ ASSERT_EQ_I(ft_isalpha('Z'), isalpha('Z'));
+	/* 11 */ ASSERT_EQ_I(ft_isalpha('Z' - 1), isalpha('Z' - 1));
+	/* 12 */ ASSERT_EQ_I(ft_isalpha('Z' + 1), isalpha('Z' + 1));
+	/* 13 */ ASSERT_EQ_I(ft_isalpha('a' + 256), isalpha('a' + 256));
+	/* 14 */ ASSERT_EQ_I(ft_isalpha('a' - 256), isalpha('a' - 256));
+	/* 15 */ ASSERT_EQ_I(ft_isalpha('a' + 1), isalpha('a' + 1));
+	/* 16 */ ASSERT_EQ_I(ft_isalpha('!'), isalpha('!'));
+	/* 17 */ ASSERT_EQ_I(ft_isalpha('{'), isalpha('}'));
+	/* 18 */ ASSERT_EQ_I(ft_isalpha('~'), isalpha('~'));
+	/* 19. zero */ ASSERT_EQ_I(ft_isalpha(0), isalpha(0));
+	// undefined: /* 20. INI_MAX */ ASSERT_EQ_I(ft_isalpha(INT_MAX), isalpha(INT_MAX));
+	// undefined: /* 21. INT_MIN */ ASSERT_EQ_I(ft_isalpha(INT_MIN), isalpha(INT_MIN));
+	/* 22 (-256~255) */ for (int i = -256; i < 256; i++) { ASSERT_EQ_I(ft_isalpha(i), isalpha(i)); }
 	return (0);
 }
